b8cefcd4d74206860439b77a11aa70f633bd6817
GregDevProjects <gdonaldsonmclean@gmail.com>
2019-05-05 14:53:56 -0300

    add sound, fix movement, upgrade unity

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,33 +6,66 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         //'generics'
         //gets a refrence to something attached to the game object
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        alignAngle();
         ProcessInput();
+    }
+
+    private void alignAngle()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
     }
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
+        Rotate();
+        Thrust();
+    }
+
+    private void Rotate()
+    {
+        //take manual control of rotation
+        rigidBody.freezeRotation = true;
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
-        } else if (Input.GetKey(KeyCode.D))
+        }
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.back);
 
+        }
+
+        rigidBody.freezeRotation = false;
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 }
