af4cdd103c3660413973f28374a020b6a4505128
GregDevProjects <gdonaldsonmclean@gmail.com>
2019-05-09 21:10:10 -0300

    a couple levels and sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 150f;
     [SerializeField] float mainThrust = 150f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip startLevel;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State {Alive, Dying, Transcending};
+    State state = State.Alive;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +24,17 @@ public class Rocket : MonoBehaviour
         //gets a refrence to something attached to the game object
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (state == State.Dying)
+        {
+           // audioSource.Stop();
+            return;
+        }
         ProcessInput();
     }
 
@@ -32,17 +46,51 @@ public class Rocket : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state !=State.Alive)
+        {
+            return;
+        }
         switch(collision.gameObject.tag)
         {
-            case "friendly":
+            case "Friendly":
                 //do nothing
                 print("okay");
                 break;
+            case "Finish":
+                StartFinish();
+                break;
             default:
-                print("dead");
+                StartDeath();
                 break;
         }
 
+    }
+
+    private void StartDeath()
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("Death", 2f); //parameterize time
+        state = State.Dying;
+    }
+
+    private void StartFinish()
+    {
+        Invoke("LoadNextScene", 1f); //parameterize time
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(startLevel);
+    }
+
+    private void Death()
+    {
+        
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
     }
 
     private void Rotate()
@@ -75,12 +123,11 @@ public class Rocket : MonoBehaviour
             rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!audioSource.isPlaying)
             {
-                audioSource.Play();
+                audioSource.PlayOneShot(mainEngine);
+            } else
+            {
+                //https://support.unity3d.com/hc/en-us/articles/206116386-How-do-I-play-multiple-Audio-Sources-from-one-GameObject-
             }
-        }
-        else
-        {
-            audioSource.Stop();
-        }
+        } 
     }
 }
